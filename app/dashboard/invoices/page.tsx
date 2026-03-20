278bff2cb2fe3d4dd899e57f9a737a6ccb843382
ammonmerrell <ammonmerrell@outlook.com>
2026-03-20 08:13:29 -0600

    corrected invoices page

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,11 +6,11 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
-import{ MetaData } from 'next';
+import{ Metadata } from 'next';
 import { metaData } from 'app/dashboard/invoices/layout'
 
 export const metadata: Metadata = {
-  title: 'create-invoices-Page',
+  title: 'Invoices',
 };
 
 export default async function Page(props:{
