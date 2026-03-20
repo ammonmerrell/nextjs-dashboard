4adb9bb588a74343092f93387465490702f75084
ammonmerrell <ammonmerrell@outlook.com>
2026-03-19 19:53:59 -0600

    added page

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,7 +2,11 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
+import { metaData } from 'app/dashboard/invoices/[id]/edit/layout'
 
+export const metadata: Metadata = {
+  title: 'edit-invoices-Page',
+};
 
 export default async function Page(props: {params: Promise<{ id: string }> }) {
   const params = await props.params;
